9ea8c1236b7cf6763dec77eccd88049706477dc7
Sulthan Syuza Prabowo <sulthanprabowo7@gmail.com>
2024-04-25 16:49:37 +0700

    added files for dataset

--- jsontransform.py
+++ jsontransform.py
@@ -1,8 +1,8 @@
 import json
 
-with open('./coedit/validation.jsonl', 'r') as infile, open('validation.jsonl', 'w') as outfile:
+with open('./data_en.jsonl', 'r') as infile, open('clean.jsonl', 'w') as outfile:
     for line in infile:
         data = json.loads(line)
-        formatted_text = f'<|user|> {data["src"]} <|assistant|> {data["tgt"]}'
+        formatted_text = f'<|begin_of_text|><|start_header_id|>user<|end_header_id|>\n {data["question"]} <|eot_id|>\n <|start_header_id|>assistant<|end_header_id|>\n {data["llama2-7b-chat_response"]} <|eot_id|>'
         json.dump({'text': formatted_text}, outfile)
         outfile.write('\n')
